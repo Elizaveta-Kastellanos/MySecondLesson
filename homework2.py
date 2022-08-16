9eb2f5bee627261dff59846ab1d03df4f12cba62
Elizaveta_Kastellanos <lizusiku28@mail.ru>
2022-08-16 14:49:48 +0300

    2 task

--- homework2.py
+++ homework2.py
@@ -1,24 +1,38 @@
 
-# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# 1.) Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример: - 6782 -> 23, - 0,56 -> 11
 
 
-
-num_str = input('Введите любое число: ')
-sum = 0
-for i in num_str:
-    if i == ',' or i == '.':
-        i = 0
-    try:
+try:
+    num_str = input('Введите любое число: ')
+    sum = 0
+    for i in num_str:
+        if i == ',' or i == '.':
+            i = 0
         i = int(i)
-    except ValueError:
-        print('Вы ввели не число!')
-        break
-    if i >= -9 and i <= 9:
-        sum = sum + i
-print(sum)
+        if i >= -9 and i <= 9:
+            sum += i
+    print(sum)
+except ValueError:
+    print('Вы ввели не число!')
 
 
+
+# 2.) Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# 
+
+
+# try:
+#     n = int(input('Введите целое число: '))
+#     multiplicate_list = []
+#     tmp = 1
+#     for i in range(1, n+1):
+#         i *= tmp
+#         multiplicate_list.append(i)
+#         tmp = i
+#     print(multiplicate_list)
+# except ValueError: 
+#     print('Вы ввели не целое число')
 
 
 
